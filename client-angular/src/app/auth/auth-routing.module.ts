680f5a27aaa4a4b5f0e70d8d0c0a11f49dd1b391
Sean <tordynamics@gmail.com>
2020-04-09 19:32:01 -0500

    typos bug fixes not complete but compiles

--- client-angular/src/app/auth/auth-routing.module.ts
+++ client-angular/src/app/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ResetPasswordComponent} from './components/reset-passowrd/reset-passowrd.component';
+import { ResetPasswordComponent} from './components/reset-password/reset-password.component';
 
 
 const routes: Routes = [
