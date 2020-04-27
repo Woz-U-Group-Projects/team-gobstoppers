17800c6ff87f6fb1be618ef9d3a3ae4aed2c26e1
Eric Steele Cesena <eric_cesena@yahoo.com>
2020-04-27 00:57:10 -0700

    updated name for route

--- client-angular/src/app/auth/auth-routing.module.ts
+++ client-angular/src/app/auth/auth-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path: 'reset-password', component: ResetPasswordComponent},
-  {path: 'bid-display', component: BidDisplayComponent}
+  {path: 'bids', component: BidDisplayComponent}
 ];
 
 @NgModule({
